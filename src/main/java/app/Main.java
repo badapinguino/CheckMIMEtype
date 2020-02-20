d6af7db462dfa7e2c8962ea61740b82afd0c3cc9
daniele.badagliacca <daniele.badagliacca@accenture.com>
2020-02-20 16:14:57 +0100

    Deleted objects that doesn't exists on the GitHub project.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -7,24 +7,16 @@ public class Main{
     public static void main(String[] args) {
         File myObj = new File("src/files/donald_duck.jpg");
         File myObj2 = new File("src/files/provatxt.txt");
-        File myObj3 = new File("src/files/Modulo_Bonus_Renzi.pdf");
         File myObj4 = new File("src/files/sample.doc");
         File myObj5 = new File("src/files/pptexamples.ppt");
         File myObj6 = new File("src/files/sample.xlsx");
         File myObj8 = new File("src/files/Sample_data.epub");
         File myObj7 = new File("src/files/zip_2MB.zip");
-        File myObj9 = new File("src/files/1576247384200_178.pdf.p7m");
-        File myObj10 = new File("src/files/1577785893607_178.docx");
-        File myObj11 = new File("src/files/1577786203796_346.txt");
-        File myObj12 = new File("src/files/ACCETTAZIONE ORDINE ACQUISTO.pdf");
-        File myObj13 = new File("src/files/DICH.LIBERAT.FOR.BENI NUOVI.xml");
         if (myObj.exists()) {
             try {
                 System.out.println("JPG " + CheckFileExtensionMime.getInstance().checkFile(myObj) );
                 System.out.println();
                 System.out.println("TXT " + CheckFileExtensionMime.getInstance().checkFile(myObj2) );
-                System.out.println();
-                System.out.println("PDF " + CheckFileExtensionMime.getInstance().checkFile(myObj3) );
                 System.out.println();
                 System.out.println("DOC " + CheckFileExtensionMime.getInstance().checkFile(myObj4) );
                 System.out.println();
@@ -35,18 +27,6 @@ public class Main{
                 System.out.println("XLSX 8: " + CheckFileExtensionMime.getInstance().checkFile(myObj8) );
                 System.out.println();
                 System.out.println("ZIP " + CheckFileExtensionMime.getInstance().checkFile(myObj7) );
-                System.out.println();
-                System.out.println("PDF.P7M " + CheckFileExtensionMime.getInstance().checkFile(myObj9) );
-                System.out.println();
-                System.out.println("PDF.P7M 2: " + CheckFileExtensionMime.getInstance().checkFile(myObj10) );
-                System.out.println();
-                System.out.println("PDF.P7M 3: " + CheckFileExtensionMime.getInstance().checkFile(myObj11) );
-                System.out.println();
-                System.out.println("PDF.P7M 4: " + CheckFileExtensionMime.getInstance().checkFile(myObj11) );
-                System.out.println();
-                System.out.println("PDF.P7M 5: " + CheckFileExtensionMime.getInstance().checkFile(myObj12) );
-                System.out.println();
-                System.out.println("PDF.P7M 6: " + CheckFileExtensionMime.getInstance().checkFile(myObj13) );
             } catch (IOException e) {
                 e.printStackTrace();
             }
